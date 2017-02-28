e53cce22473446c50d77bf0fb8136b8a167fa70d
Denis <chusovitin.den@gmail.com>
2017-02-28 11:49:50 +0300

    Change stack protection test

--- src/tests/kernel/thread/stack_protect_test.c
+++ src/tests/kernel/thread/stack_protect_test.c
@@ -20,7 +20,6 @@ static volatile int first_check;
 static volatile int second_check;
 static mmu_vaddr_t page;
 
-
 static inline int soverflow_handler(uint32_t nr, void *data) {
 	exception_flag = 1;
 
@@ -31,13 +30,30 @@ static inline int soverflow_handler(uint32_t nr, void *data) {
 }
 
 void *test_func(void *unused) {
-    char c[1];
-    c[-7000] = 1;
-    first_check = c[-7000];   // should be OK
-    page = (mmu_vaddr_t)(c - 9000) & ~VMEM_PAGE_MASK;
-    c[-9000] = 1;
-    second_check = exception_flag;
+    char c[7000];
+    page = (mmu_vaddr_t)(c - 2000) & ~VMEM_PAGE_MASK;
+    c[-2000] = 1;
     return NULL;
+}
+
+
+void *rec_func(void *arg) {
+    char arr[VMEM_PAGE_SIZE / 4];
+
+    arr[0] = 1;
+    first_check = arr[0];   // should be OK
+
+    page = (mmu_vaddr_t)(arr - VMEM_PAGE_SIZE) & ~VMEM_PAGE_MASK;
+
+    arr[-VMEM_PAGE_SIZE] = 1;
+    second_check = exception_flag;
+
+	if (!exception_flag)
+	{
+		rec_func(arg);
+	}
+
+	return NULL;
 }
 
 TEST_CASE("Stack overflow causes exception") {
@@ -48,14 +64,13 @@ TEST_CASE("Stack overflow causes exception") {
 
     pthread_t thread;
 
-    assert(!pthread_create(&thread, NULL, test_func, NULL));
+    assert(!pthread_create(&thread, NULL, rec_func, NULL));
 
     assert(!pthread_join(thread, NULL));
 
     test_assert_equal(exception_flag, 1);
 	test_assert_equal(first_check, 1);
     test_assert_equal(second_check, 1);
-
 
 	set_fault_handler(MMU_DATA_MISS, NULL);
 }
